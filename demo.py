49fc31f2b18b34da721a91d99e615576abaad4e1
Amey Joshi <ameyjoshi2001rj@gmail.com>
2021-05-21 11:13:37 +0530

    practice.

--- demo.py
+++ demo.py
@@ -1,2 +1,20 @@
 print("Hello!")
 print("Welcome to my file")
+
+# This is for learning purposes.
+message ="My name is amey nice to meet you"
+print(message)
+
+a =1
+b=2
+c = 30
+print(a/b)
+sum = a+b+c
+sub = a-b-c
+print('The value of a is {} and b is {} c is {} and sum is {} '.format(a,b,c,sum))
+
+c = 30.1
+type(c)
+print(type(a));
+
+name = input("Enter Name : ")
